9a5616d768e872dc0850e50bdf50d67fb2d36f82
walterHC <whuaracha@unsa.edu.pe>
2023-09-18 13:09:50 -0500

    REFACTOR: Commit de prueba, rama dev

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 'use client'
 
 import { Button } from '@nextui-org/react'
+import CustomSelect from '@/components/selects/CustomSelect'
 
 export default function Home() {
 	return (
 		<main>
 			<section className='relative py-40 min-h-screen w-screen h-screen'>
 				<div>
-					<Button>Click me</Button>
+					<Button onClick={() => alert("Hello world")}>Click me</Button>
+					<CustomSelect options={[{label: "dog", value: "dog"}, {label: "cat", value: "cat"}]} onChange={() => {}} />
 				</div>
 			</section>
 		</main>
