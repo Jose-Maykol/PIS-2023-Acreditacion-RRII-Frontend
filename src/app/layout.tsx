f71d4e21ff27bc73c592c510cb4e0d1f7ba69ca0
walterHC <whuaracha@unsa.edu.pe>
2023-09-29 13:18:20 -0500

    REFACTOR: comentando lineas que llaman a la interfaz de autentificacion

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({ children }: { children: React.ReactNode}) {
 		<html lang='en'>
 			<body>
 				<Providers>
-					{pathname === '/auth/login' ? null : <SideBar />}
+					{/* {pathname === '/auth/login' ? null : <SideBar />} */}
 					<main>
 						{children}
 					</main>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
 	const { push } = useRouter()
 
 	useEffect(() => {
-		push('/auth/login')
+		// push('/auth/login')
 	}, [])
 
 	return (
