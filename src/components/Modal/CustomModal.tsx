dd3eeb15ba30c309f3bf0b9db1dfb3a1151b0f64
walterHC <whuaracha@unsa.edu.pe>
2023-10-01 12:17:59 -0500

    REFACTOR: Actualizando el modal para ser mas personalidad y reutilizable

--- src/components/Modal/CustomModal.tsx
+++ src/components/Modal/CustomModal.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import { Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button} from '@nextui-org/react';
 
+interface CustomModalProps {
+  header: React.ReactNode;
+  body: React.ReactNode;
+  footer?: React.ReactNode;
+  size?: 'sm' | 'md' | 'lg' | 'xl' | '2xl';
+  classNames?: any;
+  isOpen: boolean;
+  onClose: () => void;
+}
+
 const CustomModal = ({
   header,
   body,
+  footer,
   size = 'md',
   classNames,
   isOpen,
   onClose,
-  onSubmit
 }: any) => {
 
   return (
@@ -19,18 +29,11 @@ const CustomModal = ({
       classNames={classNames}
     >
       <ModalContent>
-        {(onClose) => (
+        {() => (
           <>
             <ModalHeader className="flex flex-col gap-1">{header}</ModalHeader>
             <ModalBody>{body}</ModalBody>
-            <ModalFooter>
-                <Button color="danger" variant="flat" onPress={onClose}>
-                  Close
-                </Button>
-                <Button color="primary" onPress={onSubmit}>
-                  Sign in
-                </Button>
-              </ModalFooter>
+            <ModalFooter>{footer}</ModalFooter>
           </>
         )}
       </ModalContent>
