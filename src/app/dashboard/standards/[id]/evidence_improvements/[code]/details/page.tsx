1aa9fb95f1509b05b9f2471f0630ac3d98dc1c0d
AlexRTC1997 <aturpoco@unsa.edu.pe>
2023-10-11 22:53:56 -0500

    REFACTOR: Adding props interface for edit and details pages

--- src/app/dashboard/standards/[id]/evidence_improvements/[code]/details/page.tsx
+++ src/app/dashboard/standards/[id]/evidence_improvements/[code]/details/page.tsx
@@ -1,3 +1,9 @@
-export default function ImprovementPlanDetailsPage({ params }: any) {
+interface ImprovementPlanDetailsPageProps {
+	params: {
+		code: string
+	}
+}
+
+export default function ImprovementPlanDetailsPage({ params }: ImprovementPlanDetailsPageProps) {
 	return <h1>Improvement Plan Details with code {params.code}</h1>
 }
--- src/app/dashboard/standards/[id]/evidence_improvements/[code]/edit/page.tsx
+++ src/app/dashboard/standards/[id]/evidence_improvements/[code]/edit/page.tsx
@@ -1,3 +1,9 @@
-export default function ImprovementPlanEditPage({ params }: any) {
+interface ImprovementPlanEditPageProps {
+	params: {
+		code: string
+	}
+}
+
+export default function ImprovementPlanEditPage({ params }: ImprovementPlanEditPageProps) {
 	return <h1>Editing Improvement Plan with code {params.code}</h1>
 }
