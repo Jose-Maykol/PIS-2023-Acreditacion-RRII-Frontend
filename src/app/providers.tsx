e41bf3b04ef1cabbd7721c47f81d06133178cde2
José Maykol <jpaniura@unsa.edu.pe>
2023-10-12 17:00:57 -0500

    REFACTOR: Delete ToastProvider

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,14 +2,11 @@
 
 import React from 'react'
 import { NextUIProvider } from '@nextui-org/react'
-import { ToastProvider } from '@/hooks/ToastContext'
 
 export function Providers({ children }: { children: React.ReactNode }) {
 	return (
 		<NextUIProvider>
-			<ToastProvider>
-				{children}
-			</ToastProvider>
+			{children}
 		</NextUIProvider>
 	)
 }
