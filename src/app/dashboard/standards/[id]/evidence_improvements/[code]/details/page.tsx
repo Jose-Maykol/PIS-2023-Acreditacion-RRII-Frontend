e69b82e084c9afd345fe8907c0eb5f7f028b1d27
AlexRTC1997 <aturpoco@unsa.edu.pe>
2023-10-11 22:49:11 -0500

    TASK: Create routes for edit and details

---
+++ src/app/dashboard/standards/[id]/evidence_improvements/[code]/details/page.tsx
@@ -0,0 +1,3 @@
+export default function ImprovementPlanDetailsPage({ params }: any) {
+	return <h1>Improvement Plan Details with code {params.code}</h1>
+}
---
+++ src/app/dashboard/standards/[id]/evidence_improvements/[code]/edit/page.tsx
@@ -0,0 +1,3 @@
+export default function ImprovementPlanEditPage({ params }: any) {
+	return <h1>Editing Improvement Plan with code {params.code}</h1>
+}
