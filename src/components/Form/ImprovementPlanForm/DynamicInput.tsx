03e9aa5d8dac91b77f0a46f9c8e7a906f17203c7
AlexRTC1997 <aturpoco@unsa.edu.pe>
2023-11-02 10:39:56 -0500

    REFACTOR: InputValues CRUD

--- src/components/Form/ImprovementPlanForm/DynamicInput.tsx
+++ src/components/Form/ImprovementPlanForm/DynamicInput.tsx
@@ -1,8 +1,34 @@
 import PlusIcon from '@/components/Icons/PlusIcon'
 import { Button, Input } from '@nextui-org/react'
 import DynamicInputItem from './DynamicInputItem'
+import { ChangeEvent, useState } from 'react'
+import { planItem } from '@/types/PlanMejora'
 
 export default function DynamicInput({ identifier, label }: { identifier: string; label: string }) {
+	const [singleInputValue, setSingleInputValue] = useState('')
+	const [inputValues, setInputValues] = useState<planItem[]>([])
+
+	const handleChange = (ev: ChangeEvent<HTMLInputElement>) => {
+		setSingleInputValue(ev.target.value)
+	}
+
+	const handleAdd = () => {
+		const newInputValue = { id: Date.now(), description: singleInputValue }
+		setInputValues([...inputValues, newInputValue])
+		setSingleInputValue('')
+	}
+
+	const handleDelete = (id: number) => {
+		const updatedInputValues = inputValues.filter((item) => item.id !== id)
+		setInputValues(updatedInputValues)
+	}
+
+	const handleUpdate = (id: number, description: string) => {
+		setInputValues((prevItems) =>
+			prevItems.map((item) => (item.id === id ? { ...item, description } : item))
+		)
+	}
+
 	return (
 		<div>
 			<div className='flex items-center gap-3'>
@@ -10,6 +36,8 @@ export default function DynamicInput({ identifier, label }: { identifier: string
 					isRequired
 					id={identifier}
 					name={identifier}
+					value={singleInputValue}
+					onChange={handleChange}
 					className='mb-3'
 					label={label}
 					placeholder='Agrega uno o varios elementos'
@@ -17,13 +45,18 @@ export default function DynamicInput({ identifier, label }: { identifier: string
 					type='text'
 					variant='underlined'
 				/>
-				<Button isIconOnly color='primary' aria-label='Add' variant='solid' onClick={() => {}}>
+				<Button isIconOnly color='primary' aria-label='Add' variant='solid' onClick={handleAdd}>
 					<PlusIcon width={15} height={15} fill='fill-white' />
 				</Button>
 			</div>
 			<div>
-				{[1].map((item, i) => (
-					<DynamicInputItem key={i} />
+				{inputValues.map((item) => (
+					<DynamicInputItem
+						key={item.id}
+						inputItem={item}
+						onDelete={handleDelete}
+						onUpdate={handleUpdate}
+					/>
 				))}
 			</div>
 		</div>
--- src/components/Form/ImprovementPlanForm/DynamicInputItem.tsx
+++ src/components/Form/ImprovementPlanForm/DynamicInputItem.tsx
@@ -1,19 +1,60 @@
 import PencilIcon from '@/components/Icons/PencilIcon'
 import SaveIcon from '@/components/Icons/SaveIcon'
 import TrashIcon from '@/components/Icons/TrashIcon'
+import { planItem } from '@/types/PlanMejora'
 import { Button, Input } from '@nextui-org/react'
-import { useState } from 'react'
+import { ChangeEvent, useState } from 'react'
 
-export default function DynamicInputItem() {
+export default function DynamicInputItem({
+	inputItem,
+	onDelete,
+	onUpdate
+}: {
+	inputItem: planItem
+	onDelete: (id: number) => void
+	onUpdate: (id: number, description: string) => void
+}) {
+	const { id, description } = inputItem
 	const [isEditing, setIsEditing] = useState(false)
+	const [singleInputValue, setSingleInputValue] = useState(description)
+
+	const handleChange = (ev: ChangeEvent<HTMLInputElement>) => {
+		setSingleInputValue(ev.target.value)
+	}
+
+	const handleSaveUpdate = () => {
+		setIsEditing(!isEditing)
+
+		if (isEditing) {
+			onUpdate(id, singleInputValue)
+		}
+	}
 
 	return (
 		<div className='flex gap-2 mb-2'>
-			<Input size='sm' type='text' />
-			<Button isIconOnly color='success' aria-label='Edit' variant='flat' onClick={() => {}}>
+			<Input
+				size='sm'
+				type='text'
+				value={singleInputValue}
+				onChange={handleChange}
+				disabled={!isEditing}
+			/>
+			<Button
+				isIconOnly
+				color='success'
+				aria-label='Edit'
+				variant='flat'
+				onClick={handleSaveUpdate}
+			>
 				{isEditing ? <SaveIcon width={16} height={16} /> : <PencilIcon width={16} height={16} />}
 			</Button>
-			<Button isIconOnly color='danger' aria-label='Delete' variant='flat' onClick={() => {}}>
+			<Button
+				isIconOnly
+				color='danger'
+				aria-label='Delete'
+				variant='flat'
+				onClick={() => onDelete(id)}
+			>
 				<TrashIcon width={16} height={16} />
 			</Button>
 		</div>
