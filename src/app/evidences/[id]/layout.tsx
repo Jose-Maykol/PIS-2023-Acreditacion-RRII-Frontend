486f786b03a4d54469721601491aaa31c8872915
José Maykol <jpaniura@unsa.edu.pe>
2023-11-30 02:47:04 -0500

    REFACTOR: add download functionality for unsupported files

--- src/app/evidences/[id]/layout.tsx
+++ src/app/evidences/[id]/layout.tsx
@@ -2,6 +2,9 @@
 
 import { EvidenceService } from '@/api/Evidence/EvidenceService'
 import { useEffect, useState } from 'react'
+import { Button, Link } from '@nextui-org/react'
+import UnsupportedFileIcon from '@/components/Icons/UnsupportedFIleIcon'
+import DownloadIcon from '@/components/Icons/DownloadIcon'
 
 interface EvidenceData {
   content: string
@@ -29,12 +32,48 @@ export default function EvidencesLayout({ params }: { params: { id: number }}) {
 		'image/gif'
 	]
 
+	const handleDownload = async () => {
+		if (evidenceData) {
+			EvidenceService.downloadFile(id).then((res) => {
+				const contentDisposition = res.headers['content-disposition']
+				let filename = evidenceData.name
+				if (contentDisposition) {
+					const filenameMatch = contentDisposition.match(/filename="?(.+)"?/)
+					if (filenameMatch) {
+						filename = filenameMatch[1]
+					}
+				}
+				const url = URL.createObjectURL(res.data)
+				const link = document.createElement('a')
+				link.href = url
+				link.download = filename
+				document.body.appendChild(link)
+				link.click()
+				document.body.removeChild(link)
+				URL.revokeObjectURL(url)
+			})
+		}
+	}
+
 	if (!evidenceData) {
 		return null
 	}
 
 	if (!allowedMimeTypes.includes(evidenceData.type)) {
-		return <div className='flex flex-row items-center justify-center w-full h-screen bg-neutral-800 text-white'>Esta evidencia no se puede visualizar.</div>
+		return (
+			<div className='flex flex-col gap-4 items-center justify-center w-full h-screen bg-neutral-800 text-white'>
+				<UnsupportedFileIcon width={400} height={400} fill='fill-white' />
+				<p>Esta evidencia no se puede visualizar.</p>
+				<Button
+					color='primary'
+					startContent={<DownloadIcon width={20} height={20} fill='fill-white' />}
+					onPress={handleDownload}
+					className='font-bold'
+				>
+					Descargar documento
+				</Button>
+			</div>
+		)
 	}
 
 	return (
