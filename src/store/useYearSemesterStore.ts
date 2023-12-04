b97b51f6cb352ecd7e4d4f35f23a0195caec317e
José Maykol <jpaniura@unsa.edu.pe>
2023-12-04 11:48:05 -0500

    REFACTOR: add closing date and is closed to year semester store

--- src/store/useYearSemesterStore.ts
+++ src/store/useYearSemesterStore.ts
@@ -3,13 +3,21 @@ import { create } from 'zustand'
 type YearSemesterStore = {
   year: number | null
   semester: 'A' | 'B' | null
+  closingDate: Date | null
+  isClosed: boolean | null
   setYear (year: number | null): void
   setSemester (semester: 'A' | 'B' | null): void
+  setClosingDate (closingDate: Date | null): void
+  setIsClosed (isClosed: boolean | null): void
 }
 
 export const useYearSemesterStore = create<YearSemesterStore>((set) => ({
 	year: null,
 	semester: null,
+	closingDate: null,
+	isClosed: null,
 	setYear: (year) => set({ year }),
-	setSemester: (semester) => set({ semester })
+	setSemester: (semester) => set({ semester }),
+	setClosingDate: (closingDate) => set({ closingDate }),
+	setIsClosed: (isClosed) => set({ isClosed })
 }))
