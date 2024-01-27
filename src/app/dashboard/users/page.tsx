219c8f55c4448c9da94fc0d31c9129eb3e9aac2e
José Maykol <jpaniura@unsa.edu.pe>
2024-01-27 17:31:39 -0500

    REFACTOR: update user page and header

--- src/app/dashboard/users/page.tsx
+++ src/app/dashboard/users/page.tsx
@@ -5,7 +5,7 @@ import ContentWrapper from '@/components/ContentWrapper/ContentWrapper'
 
 export default function UsersPage() {
 	return (
-		<ContentWrapper className='bg-white h-[670px] -top-24 w-[96%] m-auto rounded-md py-5 px-10'>
+		<ContentWrapper className='bg-white h-[580px] -top-24 w-[96%] m-auto rounded-md py-5 px-10'>
 			<div className='flex w-full mb-5'>
 				<h2 className='text-xl font-semibold uppercase'>Lista de usuarios del sistema</h2>
 			</div>
