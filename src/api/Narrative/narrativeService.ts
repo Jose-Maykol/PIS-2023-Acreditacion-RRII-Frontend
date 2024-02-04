76ab151c3e767b90d09f87284c1245a36efb360a
walterHC <whuaracha@unsa.edu.pe>
2024-02-03 20:04:28 -0500

    FEATURE: adds new endpoint services

--- src/api/Narrative/narrativeService.ts
+++ src/api/Narrative/narrativeService.ts
@@ -3,26 +3,48 @@ import api from '../axios'
 
 const url = {
 	getNarrative: 'narratives',
-	updateNarrative: 'narratives',
-	deleteNarrative: 'narratives'
+	updateNarrative: 'standards/:id/narratives',
+	deleteNarrative: 'narratives',
+	enableNarrative: 'standards/:id/narratives/enable',
+	blockNarrative: 'standards/:id/narratives/block',
+	unlockNarrative: 'standards/:id/narratives/unlock'
 }
 
 export class NarrativeService extends BaseService {
 	public static async getNarrative (id: number) {
 		const { year, semester } = BaseService.getConfig()
 		const res = await api.get(`/${year}/${semester}/standards/${id}/${url.getNarrative}`)
+		console.log('get narrativa', res.data)
 		return res.data
 	}
 
-	public static async updateNarrative (id: number, params: { narrative: string}) {
+	public static async updateNarrative (id: string, params: { narrative: string}) {
 		const { year, semester } = BaseService.getConfig()
-		const res = await api.put(`/${year}/${semester}/standards/${id}/${url.updateNarrative}`, params)
+		const res = await api.put(`/${year}/${semester}/${url.updateNarrative.replace(':id', id)}`, params)
 		return res.data
 	}
 
 	public static async deleteNarrative (id: number) {
 		const { year, semester } = BaseService.getConfig()
 		const res = await api.delete(`/${year}/${semester}/standards/${id}/${url.deleteNarrative}`)
+		return res.data
+	}
+
+	public static async enableNarrative (id: string) {
+		const { year, semester } = BaseService.getConfig()
+		const res = await api.post(`/${year}/${semester}/${url.enableNarrative.replace(':id', id)}`)
+		return res.data
+	}
+
+	public static async blockNarrative (id: string) {
+		const { year, semester } = BaseService.getConfig()
+		const res = await api.post(`/${year}/${semester}/${url.blockNarrative.replace(':id', id)}`)
+		return res.data
+	}
+
+	public static async unlockNarrative (id: string) {
+		const { year, semester } = BaseService.getConfig()
+		const res = await api.post(`/${year}/${semester}/${url.unlockNarrative.replace(':id', id)}`)
 		return res.data
 	}
 }
