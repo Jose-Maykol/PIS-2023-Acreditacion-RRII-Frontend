ee8e832bac1f8ec6b466c8618ad4855f0b17a56d
walterHC <whuaracha@unsa.edu.pe>
2024-02-03 20:07:08 -0500

    FEATURE: adds global status for narrative

---
+++ src/store/useNarrativeStore.ts
@@ -0,0 +1,21 @@
+import { create } from 'zustand'
+import { Evidence } from '../types/Evidences'
+interface NarrativeStore {
+    isNarrativeEnabled: boolean
+    setNarrativeEnable: () => void
+    setNarrativeDisable: () => void
+    evidenceNarrative: Evidence | null
+    setEvidenceNarrative: (evidence: Evidence | null) => void
+    isEditingNarrative: boolean
+    setIsEditingNarrative: (value: boolean) => void
+}
+
+export const useNarrativeStore = create<NarrativeStore>((set) => ({
+	isNarrativeEnabled: false,
+	setNarrativeEnable: () => set({ isNarrativeEnabled: true }),
+	setNarrativeDisable: () => set({ isNarrativeEnabled: false }),
+	evidenceNarrative: null,
+	setEvidenceNarrative: (evidence: Evidence | null) => set({ evidenceNarrative: evidence }),
+	isEditingNarrative: false,
+	setIsEditingNarrative: (value: boolean) => set({ isEditingNarrative: value })
+}))
